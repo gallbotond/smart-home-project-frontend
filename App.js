ffcda0fe6e5082a28caa4b03b3a8bd15bb7223b3
Gáll Botond <gallbotondofficial@gmail.com>
2022-10-15 18:11:27 +0300

    create sensor page

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="ex. Living Room Temperature"
+            placeholderTextColor={"gray"}
+          />
+          <Button style={styles.button} title="Add Sensor" />
+        </View>
+        <View>
+          <Text style={styles.text}>Your Sensors:</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
   },
   text: {
-    color: "gray",
+    color: "white",
+    marginBottom: 10,
+    marginTop: 10,
   },
+  textInput: {
+    color: "white",
+    marginTop: 30
+  },
+  button: {
+    margin: 'auto'
+  },
+  view: {
+    borderColor: "green",
+    borderWidth: 2,
+    padding: 20,
+    borderRadius: 40,
+    flex: 1,
+    margin: 0
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+    alignItems: 'center',
+  }
 });
